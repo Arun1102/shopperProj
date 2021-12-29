e72cc058397a52dd220937195e7b4b86b99ee8bb
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2021-12-30 02:22:20 +0800

    second commit on slider

--- src/components/announcement.jsx
+++ src/components/announcement.jsx
@@ -3,7 +3,7 @@ import './announcement.css'
 function announcement() {
     return (
         <div className='announcement'>
-            <span>All items have 50% Discounts upon any purchse</span>
+            <span>All items have 50% Discounts upon any purchase</span>
         </div>
     )
 }
